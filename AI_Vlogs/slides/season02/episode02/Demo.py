919f5ab155f4f6810baefea2e3c45aed0d2ec12b
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2024-03-07 23:03:28 -0700

    episode02 update with video link

---
+++ AI_Vlogs/slides/season02/episode02/Demo.py
@@ -0,0 +1,30 @@
+
+x = 2
+
+x = 5
+
+y = x 
+
+x = x + 5
+
+y = x
+
+x = "AI"
+
+print(type(x))
+
+y = [ 1 ,2 ,3]
+
+x  = {1, 2, 3 }
+
+print(type(x))
+
+print(type(y))
+
+
+z = {"course": "AI" , "subject" : "Python"}
+
+print(type(z))
+
+
+help("keywords")
