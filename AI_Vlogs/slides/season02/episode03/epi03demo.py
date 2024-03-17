d7b92acb0a20bdc928ee5633a663f76dc25ee8e6
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2024-03-16 17:46:14 -0700

    season02_episode03 Update

---
+++ AI_Vlogs/slides/season02/episode03/epi03demo.py
@@ -0,0 +1,35 @@
+num1 = 100
+
+num2 = True
+
+print(num1+num2)
+
+num3 = bool(100)
+
+num4 = float(num3)
+
+print(num4 + num1)
+
+print(2 ** 5)
+
+x = 2 ** 5
+
+y = 3
+
+print(x/y)
+
+print(round(x/y,2))
+
+print(32 % 3)
+
+var2 = 100 > 10
+
+var1 = 100 < 1000
+
+print(True or False)
+
+print(True and False)
+
+print(var1 and var2)
+
+
